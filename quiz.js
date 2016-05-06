baebc11702b4bd526b37fde1c7bbfb80a866ae8c
LauraArtunduaga <Laura.Artunduaga@ge.com>
2016-05-06 15:56:16 -0400

    solving array challenge

---
+++ quiz.js
@@ -0,0 +1,31 @@
+var questArray = [
+  ['How many legs does a dog have?', 4],
+  ['How many eyes does a cat have?', 2],
+  ['How many days does a week have?', 7]
+];
+var correctAns= 0;
+var question;
+var answer;
+var html;
+
+function print(message) {
+  document.write(message);
+}
+
+for (var i = 0; i < questions.length; i += 1) {
+  question = questArray[i][0];
+  answer = questArray[i][1];
+  if (parseInt(prompt(question)) === answer) {
+    correctAns += 1;
+  }
+}
+
+if(correctAns >=2)
+{
+html = "Congrats!! You answered " + correctAns + " questions correctly.";
+}
+else
+{
+html = "Sorry :( You only got " + correctAns + " correct questions.";
+}
+print(html);
